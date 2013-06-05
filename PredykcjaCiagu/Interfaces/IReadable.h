742c91f932065132229febd7204a9725d2f9d1b9
Łukasz Fornalczyk <fornal96@o2.pl>
2013-06-05 20:31:56 +0200

    Uzupełniono interfejs o metodę ReadFile oraz niezbędne komentarze.

--- PredykcjaCiagu/Interfaces/IReadable.h
+++ PredykcjaCiagu/Interfaces/IReadable.h
@@ -1,13 +1,21 @@
 #pragma once
+#include <string>
 
 /** Interfejs umozliwiajacy odczyt danych z pliku */
 class IReadable
 {
 public:
 
-  /** Konstruktor interfejsu IReadable */
+	/** Konstruktor interfejsu IReadable */
 	IReadable(void);
 
 	/** Wirtualny destruktor interfejsu IReadable */
 	virtual ~IReadable(void);
+
+	/** Metoda czysto wirtualna za pomoca ktorej bedzie mozliwy odczyt pliku
+	@param std::string path Sciezka do pliku
+	@return true, gdy plik zostal poprawnie odczytany.
+	W przeciwnym wypadku wartoscia zwracana jest false
+	*/
+	virtual bool ReadFile(std::string path) = 0;
 };
