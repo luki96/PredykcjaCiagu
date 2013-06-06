c702f0ccad12b5aa1d9933fe1412e206ffcb6f16
Łukasz Fornalczyk <fornal96@o2.pl>
2013-06-06 17:51:48 +0200

    Poprawiono drobę błędy; zmieniono sposób usuwania obiektów z vector'a.

--- PredykcjaCiagu/Logic/NeuralNetwork.cpp
+++ PredykcjaCiagu/Logic/NeuralNetwork.cpp
@@ -3,68 +3,74 @@
 
 NeuralNetwork::NeuralNetwork(int network1, int network2, int network3)
 {
-  this->network1 = network1;
-  this->network2 = network2;
-  this->network3 = network3;
-  flag = 0;
-  
-  head =  NULL;
-  tail = NULL;
-  
+	this->network1 = network1;
+	this->network2 = network2;
+	this->network3 = network3;
+	flag = 0;
+
+	head =  NULL;
+	tail = NULL;
+
 }
 
 NeuralNetwork::~NeuralNetwork()
 {
-  while (head != NULL)
-  {
-    NeuralContainer* temp = head;
-    head = head->next;  
-    temp->neurons.erase();
-    delete temp;
-  }
+	while (head != NULL)
+	{
+		NeuronContainer* temp = head;
+		head = head->next;
+		for (int i = 0; i < temp->neurons.size(); i++)
+		{
+			delete temp->neurons.at(i);
+		}
+		delete temp;
+	}
 }
 
-NeuronContainer NeuralNetwork::CreateNetwork()
+NeuralNetwork::NeuronContainer* NeuralNetwork::CreateNetwork()
 {
-  if (head == NULL)
-  {
-    head = new NeuronContainer;
-    
-    for (int i = 0; i < network1; i++)
-    {
-      head->neurons.push_back(new Neuron(1)); 
-    }
-    
-    head->next = NULL;
-    tail = head;
-    flag = 1;
-  }
-  else if (flag == 1)
-  {
-    head->next = new NeuronContainer;
-    tail = head->next;
-    
-    for (int i =0; i < network2; i++)
-    {
-      tail->neurons.push_back(new Neuron(2));
-    }
-    
-    tail->next = NULL;
-    flag = 2;
-  }
-  else 
-  {
-    tail->next = new NeuronContainer;
-    tail = tail->next;
-    
-    for (int i =0; i < network3; i++)
-    {
-      tail->neurons.push_back(new Neuron(5));
-    }
-    
-    tail->next = NULL;
-  }
-  
-  return head;
+	if (head == NULL)
+	{
+		head = new NeuronContainer;
+
+		for (int i = 0; i < network1; i++)
+		{
+			Neuron* tmpNeuron = new Neuron(1);
+			head->neurons.push_back(tmpNeuron); 
+		}
+
+		head->next = NULL;
+		tail = head;
+		flag = 1;
+	}
+	else if (flag == 1)
+	{
+		head->next = new NeuronContainer;
+		tail = head->next;
+
+		for (int i = 0; i < network2; i++)
+		{
+			Neuron* tmpNeuron = new Neuron(2);
+			tail->neurons.push_back(tmpNeuron);
+		}
+
+		tail->next = NULL;
+		flag = 2;
+	}
+	else 
+	{
+		tail->next = new NeuronContainer;
+		tail = tail->next;
+
+		for (int i = 0; i < network3; i++)
+		{
+			Neuron* tmpNeuron = new Neuron(5);
+			tail->neurons.push_back(tmpNeuron);
+		}
+
+		tail->next = NULL;
+	}
+
+	return head;
 }
 
