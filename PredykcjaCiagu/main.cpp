afd4cea4845aa25b5d5ee68f4e02acabcf0d604e
Łukasz Fornalczyk <fornal96@o2.pl>
2013-06-06 17:53:09 +0200

    Testy czy dobrze działa tworzenie sieci neuronowej oraz sprawdzenie czy nie ma wycieków pamięci.

--- PredykcjaCiagu/main.cpp
+++ PredykcjaCiagu/main.cpp
@@ -1,15 +1,18 @@
+#define _CRTDBG_MAP_ALLOC
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <crtdbg.h>
 #include "Neuron.h"
 #include "Runner\Runner.h"
+#include "Logic\NeuralNetwork.h"
 using namespace std;
 
 int main (int argc, char* argv[])
 {
-	Runner* runner = new Runner;
-	runner->RunProgram();
-	//
-	Neuron* n = new Neuron(5);
+	//Runner* runner = new Runner;
+	//runner->RunProgram();
+	//Neuron* n = new Neuron(5);
 	//vector<double> tab;
 	//tab.push_back(0.1);
 	//tab.push_back(0.3);
@@ -19,8 +22,17 @@ int main (int argc, char* argv[])
 	//n->CalculateNeuronInputsWeights();
 	//n->CalculateNeuronOutputFunction(tab);
 	
-	delete runner;
-	delete n;
+	//delete runner;
+	//delete n;
+
+	NeuralNetwork* nn = new NeuralNetwork(1, 2, 5);
+	nn->CreateNetwork();
+	nn->CreateNetwork();
+	nn->CreateNetwork();
+	nn->~NeuralNetwork();
+
+	delete nn;
+	_CrtDumpMemoryLeaks();
 	system("pause");
 	return 0;
 }
