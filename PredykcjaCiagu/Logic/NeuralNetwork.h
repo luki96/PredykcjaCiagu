6ab96151b457606954891b18e1b4e86bf490b344
luki96 <fornal96@o2.pl>
2013-06-06 14:20:34 +0200

    Update NeuralNetwork.h

--- PredykcjaCiagu/Logic/NeuralNetwork.h
+++ PredykcjaCiagu/Logic/NeuralNetwork.h
@@ -1,8 +1,27 @@
 #pragma once
 #include "Neuron.h"
+#include <vector>
 
 class NeuralNetwork
 {
   private: 
+  
+  int network1, network2, network3, flag;
+  
+  struct NeuronContainer
+  {
+    std::vector<Neuron> neurons;
+    NeuronContainer* next;
+  };
+  
+  NeuronContainer* head;
+  NeuronContainer* tail;
+  
+  NeuronContainer* CreateNetwork();
+  
+  public:
+  
+  NeuralNetwork(int, int, int);
+  ~virtual NeuralNetwork();
   
 };
