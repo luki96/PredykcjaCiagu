120af351caa0a13c62e87b86bb098c4c472eb7ed
Ja <myland@interia.pl>
2013-06-17 17:50:40 +0200

    Wstępnie ukończono funkcja ucząca sieć neuronową. Powinna Przyjmować wartości, obliczać wagi, zapisywać wstępne wyniki. (Brakuje wstecznej propagacji błędu).

--- PredykcjaCiagu/Logic/NeuralNetworkComponents/BackPropagation.cpp
+++ PredykcjaCiagu/Logic/NeuralNetworkComponents/BackPropagation.cpp
@@ -12,12 +12,73 @@ BackPropagation::~BackPropagation(void)
 
 NeuralNetwork* BackPropagation::BackPropagationMethod(int x, int y, NeuralNetwork* net)
 {
-	//TODO: Dostaæ siê do elementów wskazywanych przez tablice przechowywan¹ w strukturze, zawartej w NeuralNetwork
-
+	int NETWORK_ONE = 2, NETWORK_TWO = 5, NETWORK_THREE = 1;
 	int i = 0;
+	int tab = 0;
+	std::vector<double> neuronsResults;
+	double firstREsult = 0;
 
-	net->head->neurons[i];
+	while (i < NETWORK_ONE)
+	{
+		std::vector<double> parameters;
 
+		if (i == 0)
+		{
+			parameters[0] = x;
+		}
+		else if (i == 1)
+		{
+			parameters[0] = y;
+		}
+		else
+		{
+			parameters[0] = 0;
+		}
+
+		net->head->neurons[i]->CalculateNeuronOutputFunction(parameters);
+		neuronsResults[tab] = net->head->neurons[i]->GetOutputFunction();
+
+		i++;
+		tab++;
+	}
+
+	i = 0;
+
+	while (i < NETWORK_TWO)
+	{
+		std::vector<double> parameters;
+
+		parameters[0] = neuronsResults[0];
+		parameters[1] = neuronsResults[1];
+
+		net->head->next->neurons[i]->CalculateNeuronOutputFunction(parameters);
+		neuronsResults[tab] = net->head->next->neurons[i]->GetOutputFunction();
+
+		i++;
+		tab++;
+	}
+
+	i = 0;
+
+	while (i < NETWORK_THREE)
+	{
+		int count = 0;
+		std::vector<double> parameters;
+
+		while (count < tab)
+		{
+			parameters[count] = neuronsResults[count];
+			count ++;
+		}
+
+		net->tail->neurons[i]->CalculateNeuronOutputFunction(parameters);
+		neuronsResults[tab] = net->tail->neurons[i]->GetOutputFunction();
+
+		i++;
+		tab++;
+	}
+
+	firstREsult = neuronsResults[tab-1];
 
 	//TODO: Algorytm do propagacji, po uzyskaniu wstêpnego wyniku 
 	
@@ -27,6 +88,8 @@ NeuralNetwork* BackPropagation::BackPropagationMethod(int x, int y, NeuralNetwor
 double BackPropagation::NetworkResult(int x, int y, NeuralNetwork* net)
 {
 	double netResult = 0;
+
+	//TODO: Obliczanie wyniku koñcowego
 
 	return netResult; 
 }
