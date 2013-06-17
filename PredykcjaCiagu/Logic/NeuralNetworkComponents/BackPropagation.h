7ea4a06f8da1c80c78cc0939b4674bcb4e9ab33d
Ja <myland@interia.pl>
2013-06-17 20:49:32 +0200

    Dodano konwersję danych w klasie BackPropagation.
    Projekt kompiluje się, lecz nie działa.

--- PredykcjaCiagu/Logic/NeuralNetworkComponents/BackPropagation.h
+++ PredykcjaCiagu/Logic/NeuralNetworkComponents/BackPropagation.h
@@ -2,12 +2,16 @@
 #include "Neuron.h"
 #include "NeuralNetwork.h"
 #include "..\..\Interfaces\IPropagable.h"
-
+#include "..\..\Logic\DataConverter.h"
 
 /** Klasa dziedzicz¹ca interfejs IPropagable
 	s³u¿¹ca do wykonania propagacji wstecznej */ 
 class BackPropagation: public IPropagable
 {
+private:
+
+		DataConverter* converter;
+
 public:
 	/** Konstruktor bezargumentowy klasy BackPropagation */ 
 	BackPropagation(void);
